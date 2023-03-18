6a0bce6db6964f6fb52530b7bdb9e5d8c757a95f
Chad Chapman <chad.chapman2010@gmail.com>
2023-03-17 20:20:34 -0400

    Manual testing.

--- public/app.js
+++ public/app.js
@@ -4,21 +4,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
  * IMPORTS                                                                    *
  *****************************************************************************/
 const LinkedList_1 = require("./LinkedList");
-const TestElement_1 = require("./TestElement");
-const linkedList = LinkedList_1.LinkedList.fromValues(10, 20);
-const element = new TestElement_1.TestElement(30, 3);
-linkedList.append(element);
+const linkedList = LinkedList_1.LinkedList.fromValues(50, 40, 30, 20, 10, 0);
 console.log(linkedList.toString());
-const first = linkedList.getFirst();
-console.log(`first: ${first.data.getKey()}`);
-const last = linkedList.getLast();
-console.log(`Last: ${last === null || last === void 0 ? void 0 : last.data.getKey()}`);
-console.log(linkedList.size());
-linkedList.removeLast();
-linkedList.removeLast();
-linkedList.removeLast();
+linkedList.selectionSort();
 console.log(linkedList.toString());
-console.log(linkedList.size());
-linkedList.append(element);
-console.log(linkedList.toString());
-console.log(linkedList.size());
--- src/app.ts
+++ src/app.ts
@@ -6,23 +6,9 @@ import { TestElement } from "./TestElement";
 
 
 
-const linkedList = LinkedList.fromValues(10, 20);
-const element = new TestElement(30, 3);
-linkedList.append(element);
-console.log(linkedList.toString());
+const linkedList = LinkedList.fromValues(50, 40, 30, 20, 10, 0);
 
-const first = linkedList.getFirst();
-console.log(`first: ${first.data.getKey()}`);
 
-const last = linkedList.getLast();
-console.log(`Last: ${last?.data.getKey()}`);
-
-console.log(linkedList.size());
-linkedList.removeLast();
-linkedList.removeLast();
-linkedList.removeLast();
 console.log(linkedList.toString());
-console.log(linkedList.size());
-linkedList.append(element);
+linkedList.selectionSort();
 console.log(linkedList.toString());
-console.log(linkedList.size());

75991e3f1ab6c778292bb0019d23f5410e2989a0
Chad Chapman <chad.chapman2010@gmail.com>
2023-03-18 12:59:53 -0400

    Manual testing.

--- public/app.js
+++ public/app.js
@@ -4,7 +4,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
  * IMPORTS                                                                    *
  *****************************************************************************/
 const LinkedList_1 = require("./LinkedList");
-const linkedList = LinkedList_1.LinkedList.fromValues(50, 40, 30, 20, 10, 0);
-console.log(linkedList.toString());
+const TestElement_1 = require("./TestElement");
+let length = 20;
+const linkedList = new LinkedList_1.LinkedList();
+for (let i = 0; i < length; i++) {
+    let element = new TestElement_1.TestElement(Math.floor(Math.random() * 100), i + 1);
+    linkedList.append(element);
+}
+console.log(`Original list: ${linkedList.toString()}`);
 linkedList.mergeSort(linkedList.getFirst());
-console.log(linkedList.toString());
+console.log(`Sorted list: ${linkedList.toString()}`);
--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,15 @@
 import { LinkedList } from "./LinkedList";
 import { TestElement } from "./TestElement";
 
+let length = 20;
+
+const linkedList = new LinkedList();
+for (let i = 0; i < length; i++) {
+    let element = new TestElement(Math.floor(Math.random() * 100), i + 1);
+    linkedList.append(element);
+}
 
 
-const linkedList = LinkedList.fromValues(50, 40, 30, 20, 10, 0);
-
-
-console.log(linkedList.toString());
+console.log(`Original list: ${linkedList.toString()}`);
 linkedList.mergeSort(linkedList.getFirst());
-console.log(linkedList.toString());
+console.log(`Sorted list: ${linkedList.toString()}`);
